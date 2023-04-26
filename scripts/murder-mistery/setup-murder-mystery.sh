5f7a7017e25e85124cc7846e3d5bf0eb7d2b5713
Jan Simson <git@simson.io>
2023-04-26 16:55:43 +0200

    Also overwrite commiter date (next to author date)

--- scripts/murder-mistery/setup-murder-mystery.sh
+++ scripts/murder-mistery/setup-murder-mystery.sh
@@ -9,24 +9,24 @@ git init
 cp ../1973-04-26/news.md ./news.md
 cp ../1973-04-26/image.jpg ./image.jpg
 git add .
-git commit --date="1973-04-26" -m "Wealthy Businessman Found Murdered in Colchester Mansion (1973)"
+GIT_COMMITTER_DATE="1973-04-26T00:00:00" git commit --date="1973-04-26" -m "Wealthy Businessman Found Murdered in Colchester Mansion (1973)"
 
 cp ../1976-11-12/news.md ./news.md
 cp ../1976-11-12/image.jpg ./image.jpg
 git add .
 
-git commit --date="1976-11-12" -m "Shocking Murder of Young Woman Rocks Colchester Community (1976)"
+GIT_COMMITTER_DATE="1976-11-12T00:00:00" git commit --date="1976-11-12" -m "Shocking Murder of Young Woman Rocks Colchester Community (1976)"
 
 cp ../1977-08-06/news.md ./news.md
 cp ../1977-08-06/image.jpg ./image.jpg
 git add .
-git commit --date="1977-08-06" -m "Violent Crime Wave Continues in Colchester as Another Citizen Falls Victim (1977)"
+GIT_COMMITTER_DATE="1977-08-06T00:00:00" git commit --date="1977-08-06" -m "Violent Crime Wave Continues in Colchester as Another Citizen Falls Victim (1977)"
 
 # Remove news
 rm ./news.md
 rm ./image.jpg
 git add .
-git commit --date="1990-01-01" -m "Hide evidence: Remove all old news reports"
+GIT_COMMITTER_DATE="1990-01-01T00:00:00" git commit --date="1990-01-01" -m "Hide evidence: Remove all old news reports"
 
 # Add last repo information e.g. README
 cp ../last/detective.jpg detective.jpg
@@ -35,5 +35,5 @@ git add .
 git commit -m "Add README / Instructions to Solve Mystery"
 
 # Optional: Push to github
-# git remote add git@github.com:jansim/git-murder-mystery.git
-# git push --set-upstream origin main
+git remote add origin git@github.com:jansim/git-murder-mystery.git
+git push --set-upstream origin main
